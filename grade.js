3e411d37d2567d3de2210ad052c63bfe35b0346f
anitah254-pixel <ann.kilonzo@student.moringaschool.com>
2024-07-11 04:51:50 -0700

    Add files via upload

--- grade.js
+++ grade.js
@@ -1,18 +1,20 @@
-const prompt = require('prompt-sync)();
+const prompt = require('prompt-sync')();
+
 function studentMarks(){
     let marks = parseFloat(prompt('Enter students marks:'));
-    if(marks > 79 $$marks <= 100){
-        console.log('A');
-    } else if(marks >= 60 $$marks <= 79){
-        console.log('B');
-    } else if(marks > 49 $$marks <= 59){
-        console.log('C');
-    } else if(marks >= 40 $$marks <= 49){
-        console.log('D');
-    } else if(marks >=0 $$marks < 40){
-        console.log('E');
+    if(marks > 79 &&marks <= 100){
+        console.lo('A');
+    } else if(marks >= 60 &&marks <= 79){
+        console.lo('B');
+    } else if(marks > 49 &&marks <= 59){
+        console.lo('C');
+    } else if(marks >= 40 &&marks <= 49){
+        console.lo('D');
+    } else if(marks >=0 &&marks < 40){
+        console.lo('E');
     } else{
-        console.log('invalid')
+        console.lo('invalid')
     }
 }
+
 studentMarks();
