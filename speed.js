dd440cf90fb4ed091e9590cbf0dbe1090fde2c87
anitah254-pixel <ann.kilonzo@student.moringaschool.com>
2024-07-11 05:26:28 -0700

    Add files via upload

--- speed.js
+++ speed.js
@@ -4,17 +4,18 @@ function speedDetector(){
     let speed = parseFloat(prompt('Enter speed of the car:'));
 
     if(speed < 70){
-        console.log('Ok);
-        return'Ok';
+        console.log('Ok');
+            return'Ok';
     } else{
         let point = Math.floor((speed -70)/5);
         if(point > 12){
             console.log('license suspended');
             return'license suspended';
         } else{
-            console.log(Your demerit points are: ${points});
-            return Your demerit points are: ${points};
+            console.log('Your demerit points are: ${points}');
+            return' Your demerit points are: &{points}';
         }
     }
 }
+
 console.log(speedDetector());
